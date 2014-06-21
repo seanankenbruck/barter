128ec11c75a87ca68a4c4aa3aeaca3583f0e92a8
Sean Ankenbruck <smankenb@gmail.com>
2014-06-21 16:34:04 -0400

    welcome page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BarterRaleigh::Application.config.secret_key_base = 'aee81129968e0dd4371ead82e53fe6f4834652ba12000aba24d7dd187eef6e9a02297fc4d82ff94ef69409353cd51e02a3c8f11be5319ec0b1b1843f77f688b9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BarterRaleigh::Application.config.session_store :cookie_store, key: '_barter_raleigh_session'
