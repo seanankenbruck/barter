8b9df22d4d9ab49930cdf4b7cb9a46cda0b0291d
Sean Ankenbruck <smankenb@gmail.com>
2014-06-22 23:46:47 -0400

    uninitialized constant

--- app/controllers/contact_form_controller.rb
+++ app/controllers/contact_form_controller.rb
@@ -5,7 +5,7 @@ class ContactFormController < ApplicationController
 
   def create
   	begin
-  		@contact_form = ContactForm.new(params[:contact_form])
+  		@contact_form = ContactForm.new
   		@contact_form.request = request
   		if @contact_form.deliver
   			flash.now[:notice] = 'Thank you for the message!'
